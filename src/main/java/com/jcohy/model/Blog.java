b4bfe24fa1ae740e79afc80cd261cf8d27abb36d
Allen <15332622786@163.com>
2017-12-16 01:17:18 +0800

    添加目录

---
+++ src/main/java/com/jcohy/model/Blog.java
@@ -0,0 +1,12 @@
+package com.jcohy.model;
+
+import java.io.Serializable;
+
+/**
+ * @Author: Allen Liu
+ * @Description:
+ * @Date: Created in 1:16 2017/12/16
+ * @Modified By:
+ **/
+public class Blog implements Serializable {
+}
